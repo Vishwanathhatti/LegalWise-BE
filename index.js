5d82bddaab3f931b37279b008cfd812b408018a8
Vishwanathhatti <vishwanathhatti30@gmail.com>
2025-05-19 00:27:29 +0530

    Added frontend url in cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption={
-    origin:'https://darling-piroshki-2ac239.netlify.app',
+    origin:'https://legalwise.netlify.app',
     credentials:true,
 }
 app.use(cors(corsOption));
